46cac446fa95f4717b2336aefe9ba9f61bdcd298
dongmin7208 <sayg3o3d@gmail.com>
2021-08-08 13:51:41 +0900

    Add back server setting

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -6,8 +6,8 @@ const passport = require("passport");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const path = require("path");
-// const hpp = require('hpp');
-// const helmet = require('helmet');
+const hpp = require("hpp");
+const helmet = require("helmet");
 const postRouter = require("./routes/post");
 const postsRouter = require("./routes/posts");
 const userRouter = require("./routes/user");
@@ -25,10 +25,18 @@ db.sequelize
   })
   .catch(console.error);
 passportConfig();
+if (process.env.NODE_ENV === "production") {
+  app.use(morgan("combined"));
+  app.use(hpp());
+  app.use(helmet());
+} else {
+  app.use(morgan("dev"));
+}
+
 app.use(morgan("dev"));
 app.use(
   cors({
-    origin: "http://localhost:3060", //* 안된다. 프론트와백엔드간에 민감한 정보를 보내니까. 정확한 주소를 적어달라 에러
+    origin: ["http://localhost:3060", "nodebird.com"], //* 안된다. 프론트와백엔드간에 민감한 정보를 보내니까. 정확한 주소를 적어달라 에러
     credentials: true, //쿠키를 같이 전달하고싶으면 true로 만들어주면뎀.
   })
 );
